a9df8482c21103ac3b46c1c364d620c83a1fcb17
rowlesmr <rowlesmr@gmail.com>
2021-01-14 14:09:53 +0800

    Update tt_artist.R
    fixed fct_lump!

--- 2021_03/tt_artist.R
+++ 2021_03/tt_artist.R
@@ -1,6 +1,3 @@
-install.packages("ggrepel")
-
-
 
 library(tidytuesdayR)
 library(tidyverse)
@@ -37,19 +34,19 @@ plain <- function(x,...) {
 }
 
 
-#I should be able to use fct_lump_* in here to join them together, but I can't figure it out.
+#Got the lump to work!
 artwork %>% 
-  count(artist) %>% 
-  mutate(artist = if_else(n < 100, "Other", artist)) %>% #this should be a fct_lump_* function
-  mutate(artist = fct_reorder(artist,n)) %>% 
-  group_by(artist) %>% 
-  summarise(count = sum(n)) %>% 
-ggplot(aes(x = artist,y = count))+
+  mutate(artist = fct_lump_n(artist,50,other_level = "Everyone else"))%>% 
+  count(artist) %>%   
+                  #reorder the factors by how many occurances there are, and
+                  #  then make the "everyone else" category go to the end of the list
+  mutate(artist = fct_reorder(artist,n) %>% fct_relevel("Everyone else", after = 0)) %>% 
+ggplot(aes(x = artist,y = n))+
   geom_col(fill = "#bbd1f0")+
   scale_y_log10(minor_break = log10_minor_break(), labels = plain)+
   coord_flip(ylim=c(100,40000))+
-  labs(y = "Number of pieces by artist", x= "Artists with more than 100 pieces",
-       title = "Art in the Tate", 
+  labs(y = "Number of pieces by artist", x= "",
+       title = "Top 50 artists in the Tate collection", 
        subtitle = "Joseph Turner et al.", 
        caption = "Data from the Tate Art Museum. Visualisation by Matthew Rowles | @xray_matt") +
   theme_classic() +
@@ -63,16 +60,20 @@ ggplot(aes(x = artist,y = count))+
         panel.grid.minor.x = element_line(color = "grey20"),
         axis.line = element_line(color = "#ced8f2"),
         axis.ticks = element_line(color = "#ced8f2"),
-        axis.text.y = element_text(size = 11, colour = "#9dc6e0"),
+        axis.text.y = element_text(size = 10, colour = "#9dc6e0"),
         axis.text.x = element_text(size = 13, colour = "#9dc6e0"), 
         axis.title = element_text(size = 13),        
         plot.title = element_text(size = 21),
         plot.subtitle = element_text(size = 13),
         plot.caption = element_text(size = 11)       
-        
-  ) 
+        ) 
 
 ggsave("tate_artists.png", type = "cairo-png", dpi = 400)
 
+
+
+artwork %>% filter(artist != "Turner, Joseph Mallord William") %>% 
+  count(artist, medium) %>% 
+  arrange(desc(n))
 
 
